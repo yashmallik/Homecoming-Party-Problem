7e97a2e3df813e6693fda07ebf3b4e263fa759eb
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-08-26 22:32:07 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,19 +1,35 @@
 // Homecoming Party
 
-Problem Description
+// Problem Description
 
-The original family in the town has organised a homecoming party with N people invited.
+// The original family in the town has organised a homecoming party with N people invited.
 
-Each person has a special trust value denoted by array A. A person i will be friends with a person j only if either A[i]%A[j] ==0 or A[j]%A[i]==0.
+// Each person has a special trust value denoted by array A. A person i will be friends with a person j only if either A[i]%A[j] ==0 or A[j]%A[i]==0.
 
-Find the maximum number of friends each person can make in this party.
+// Find the maximum number of friends each person can make in this party.
 
-Problem Constraints
+// Problem Constraints
 
-1<N<= 2 x 10³
+// 1<N<= 2 x 10³
 
-1 <=A[i]<= 10³
+// 1 <=A[i]<= 10³
 
-Input Format
+// Input Format
 
-1st and only arguement has an integer array A
+// 1st and only arguement has an integer array A
+
+----------------X-------------------------------X----------------------------X-------------------------------------X---------------------------------------------X-------------------
+
+  
+  
+let a = [];
+let count = 0;
+for (let i = 0 ; i <A.length; i++){
+	for (let j = 0 ; j <A.length; j++){
+		if(A[i]%A[j]==0 ||A[i]%A[j]==0){
+        count++;
+	}
+    a.push(count)
+
+	}
+}return a
