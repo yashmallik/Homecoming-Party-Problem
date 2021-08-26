d1a3fa1aac0de329698d5364bb58b9be6bdbcc9f
yashmallik <76205209+yashmallik@users.noreply.github.com>
2021-08-26 22:29:59 +0530

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,19 @@
+// Homecoming Party
+
+Problem Description
+
+The original family in the town has organised a homecoming party with N people invited.
+
+Each person has a special trust value denoted by array A. A person i will be friends with a person j only if either A[i]%A[j] ==0 or A[j]%A[i]==0.
+
+Find the maximum number of friends each person can make in this party.
+
+Problem Constraints
+
+1<N<= 2 x 10³
+
+1 <=A[i]<= 10³
+
+Input Format
+
+1st and only arguement has an integer array A
